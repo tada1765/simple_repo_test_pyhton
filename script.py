922cefce4001fc4e392efe33e0b8d77bbc814f1a
tada1765 <www.tada1765@gmail.com>
2019-10-01 16:04:37 +0800

    modify code

--- script.py
+++ script.py
@@ -2,18 +2,21 @@ import sys
 
 import requests
 
-name = input("Your name? ")
-print("Hello," , name)
+# name = input("Your name? ")
+# print("Hello," , name)
 
 # print(sys.version)
 # print(sys.executable)
 
-# def greet(who_to_greet):
-#     greeting = "Hello, {}".format(who_to_greet)
-#     return greeting
 
- #print(greet("World"))
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
 
- #print(greet("Corey"))
+
+result = "hi"
+print(greet("World"))
+print(greet("wq"))
+
 # r = requests.get('https://coreyms.com')
- #print(r.status_code)
+# print(r.status_code)
